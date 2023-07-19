8499e2f4825dbe308775d4c33420091e921a33aa
Alireza Habibzadeh <alirezahabib80@gmail.com>
2023-07-19 15:34:04 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='dscript-driver',
+    version='0.1.0',
+    author='LPQM',
+    description='A Python driver for the dScript based switches',
+    packages=['dscript'],
+    install_requires=[],
+)
