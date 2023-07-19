747c6c3982176e2eee98a0c36548dc89fe2501b2
Alireza Habibzadeh <alirezahabib80@gmail.com>
2023-07-19 16:06:33 +0200

    Fix dscript config not working

--- dscript/__init__.py
+++ dscript/__init__.py
@@ -4,9 +4,20 @@ from time import sleep as _sleep
 
 import requests
 
-from config import *
-
 logging.basicConfig(level=logging.INFO)
+
+# Don't modify these values here (unless you want to change the defaults)
+# modify them in your script (after the import)
+pulse_duration = 0.050  # in seconds
+slowdown = 0.010  # in seconds
+settle_down = 0.500  # in seconds
+ip_address = '169.254.100.1'
+port = 80
+connection_timeout = 4.0  # in seconds
+
+
+def toggle_url(relay):
+    return 'http://' + ip_address + '/dscript.cgi?V20552=' + str(relay)
 
 
 def _fetch() -> dict[str, str]:
