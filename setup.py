5b07cf3733730429f75ae758d8c74be3142983ee
Alireza Habibzadeh <alirezahabib80@gmail.com>
2023-07-19 15:34:35 +0200

    Add EPFL to author

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='dscript-driver',
     version='0.1.0',
-    author='LPQM',
+    author='LPQM-EPFL',
     description='A Python driver for the dScript based switches',
     packages=['dscript'],
     install_requires=[],
